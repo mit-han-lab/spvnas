5d1383ebc44c695e402e12f492e1bfb27f38c66f
Sandeep Menon <sandeepmenon@deepen.ai>
2020-12-03 18:41:57 +0530

    removed input_labels

--- spvnas/inference.py
+++ spvnas/inference.py
@@ -16,29 +16,17 @@ from torchsparse.utils import sparse_quantize, sparse_collate
 from torchsparse import SparseTensor
 from model_zoo import spvcnn
 
-def process_point_cloud(input_point_cloud, input_labels=None, voxel_size=0.05):
+
+def process_point_cloud(input_point_cloud, voxel_size=0.05):
     input_point_cloud[:, 3] = input_point_cloud[:, 3]
     pc_ = np.round(input_point_cloud[:, :3] / voxel_size)
     pc_ -= pc_.min(0, keepdims=1)
-    
-    label_map = create_label_map()
-    if input_labels is not None:
-        labels_ = label_map[input_labels & 0xFFFF].astype(
-            np.int64)  # semantic labels
-    else:
-        labels_ = np.zeros(pc_.shape[0], dtype=np.int64)
-    
+
+    labels_ = np.zeros(pc_.shape[0], dtype=np.int64)
     feat_ = input_point_cloud
-    
-    if input_labels is not None:
-        out_pc = input_point_cloud[labels_ != labels_.max(), :3]
-        pc_ = pc_[labels_ != labels_.max()]
-        feat_ = feat_[labels_ != labels_.max()]
-        labels_ = labels_[labels_ != labels_.max()]
-    else:
-        out_pc = input_point_cloud
-        pc_ = pc_
-        
+    out_pc = input_point_cloud
+    pc_ = pc_
+
     inds, labels, inverse_map = sparse_quantize(pc_,
                                                 feat_,
                                                 labels_,
@@ -46,11 +34,11 @@ def process_point_cloud(input_point_cloud, input_labels=None, voxel_size=0.05):
                                                 return_invs=True)
     pc = np.zeros((inds.shape[0], 4))
     pc[:, :3] = pc_[inds]
-    
+
     feat = feat_[inds]
     labels = labels_[inds]
     lidar = SparseTensor(
-        torch.from_numpy(feat).float(), 
+        torch.from_numpy(feat).float(),
         torch.from_numpy(pc).int()
     )
     return {
@@ -63,7 +51,6 @@ def process_point_cloud(input_point_cloud, input_labels=None, voxel_size=0.05):
 
 
 def get_inference(model, point_cloud):
-        
     if torch.cuda.is_available():
         device = 'cuda:0'
     else:
@@ -91,12 +78,13 @@ def get_inference(model, point_cloud):
 
     return predictions.astype(np.int32)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default='SemanticKITTI_val_SPVCNN@119GMACs')
     parser.add_argument('--file', type=str, default=None)
     args = parser.parse_args()
-    
+
     point_cloud_name = args.file
     pc = np.fromfile(point_cloud_name, dtype=np.float32).reshape(-1, 4)
     get_inference(args.model, pc)
